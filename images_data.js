fb4cc51f11f506769e738aa2535e42d4530b78c5
ahmedabad23804-ship-it <ahmedabad23804@gmail.com>
2025-12-30 12:05:47 +0200

    Create IMAGES_DB with subject image mappings
    Added a database of image paths for various subjects.

---
+++ images_data.js
@@ -0,0 +1,13 @@
+const IMAGES_DB = {
+    "لغة عربية": "images/arabic.jpg",
+    "English": "images/english.jpg",
+    "فيزياء": "images/physics.jpg",
+    "كيمياء": "images/chemistry.jpg",
+    "أحياء": "images/biology.jpg",
+    "رياضيات": "images/math.jpg",
+    "تاريخ": "images/history.jpg",
+    "جغرافيا": "images/geo.jpg",
+    "فرنساوي": "images/french.jpg",
+    "جيولوجيا": "images/geo.jpg", 
+    "default": "images/english.jpg"
+};
